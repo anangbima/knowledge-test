84cb835e5ed4632ac9986ad6abc4eeb81984bd04
anangbima <50908265+anangbima@users.noreply.github.com>
2024-10-01 16:27:54 +0700

    Folder Management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './router/root.jsx'
+import './styles/global.scss'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <RouterProvider router={router} />
+  </>,
 )
